140f2cd73a8d4603ccedf408c821c66eb0487965
Jessica <jessicamwangi933@gmail.com>
2022-02-08 08:38:08 +0300

    added an output, resetfields

--- src/app/make-quotes/make-quotes.component.ts
+++ src/app/make-quotes/make-quotes.component.ts
@@ -8,6 +8,22 @@ import {  Quote } from '../quotes';
 })
 export class MakeQuotesComponent implements OnInit {
 
+  newQuote = new Quote(0, '', '', '', new Date(), 0, 0);
+
+  @Output() addQuote = new EventEmitter<Quote>()
+
+  submitQuote() {
+
+    this.addQuote.emit(this.newQuote);
+
+  }
+  resetFields() {
+    this.newQuote.author = '';
+    this.newQuote.quote = '';
+    this.newQuote.user = '';
+
+  }
+
   constructor() { }
 
   ngOnInit(): void {
