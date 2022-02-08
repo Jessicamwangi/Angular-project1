dc63ae44aafebeb3db53d531a7ac0d859e1d8be6
Jessica <jessicamwangi933@gmail.com>
2022-02-08 19:16:27 +0300

    added user to quote

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,10 +9,10 @@ import {  Quote } from '../quote';
 export class QuotesComponent implements OnInit {
 
     Quotes: Quote[] = [
-    new Quote(0, "If you want to live a happy life, tie it to a goal, not to people or things.", "Albert Einstein", new Date(2022, 2, 5), 0, 0),
-    new Quote(1, "Money and success don’t change people; they merely amplify what is already there.", "Will  Smith", new Date(2022, 2, 6), 0, 0),
-    new Quote(2, "Turn your wounds into wisdom.",  "Oprah Winfrey", new Date(2022, 2, 7), 0, 0),
-    new Quote(3, "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking",  "Steve Jobs", new Date(2022, 2, 8), 0, 0),
+    new Quote(0, "If you want to live a happy life, tie it to a goal, not to people or things.", "Albert Einstein","John Jacob", new Date(2022, 2, 5), 0, 0),
+    new Quote(1, "Money and success don’t change people; they merely amplify what is already there.", "Will  Smith","Rebecca ", new Date(2022, 2, 6), 0, 0),
+    new Quote(2, "Turn your wounds into wisdom.",  "Oprah Winfrey","Hosea" ,new Date(2022, 2, 7), 0, 0),
+    new Quote(3, "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking",  "Steve Jobs","Felix", new Date(2022, 2, 8), 0, 0),
 
   ];
 
