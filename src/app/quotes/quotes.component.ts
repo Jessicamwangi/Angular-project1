c52a57a04ef5403ae093a7e50f27df5606d7be9a
Jessica <jessicamwangi933@gmail.com>
2022-02-08 06:47:47 +0300

    added quotes, authors and dates

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
+  mQuotes: Quote[] = [
+    new Quote(0, "If you want to live a happy life, tie it to a goal, not to people or things.", "Albert Einstein", new Date(2022, 2, 5), 0, 0),
+    new Quote(1, "Money and success don’t change people; they merely amplify what is already there.", "Will  Smith", new Date(2022, 2, 6), 0, 0),
+    new Quote(2, "Turn your wounds into wisdom.",  "Oprah Winfrey", new Date(2022, 2, 7), 0, 0),
+    new Quote(3, "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking",  "Steve Jobs", new Date(2022, 2, 8), 0, 0),
+
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
