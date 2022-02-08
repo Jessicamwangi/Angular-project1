5b91e23e7bd42ad7d70b3ce132475847441a612a
Jessica <jessicamwangi933@gmail.com>
2022-02-08 08:46:14 +0300

    added a constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,15 @@
 export class Quote {
+  showDetails: boolean
+    constructor(
+        public id: number,
+        public quote: string,
+        public author: string,
+        public user: string,
+        public date: Date,
+        public upvotes: number,
+        public downvotes: number) {
+
+            this.showDetails = false;
+            
+    }
 }
